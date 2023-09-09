487c74bafd03585367dd740ef09b66a4b65facf9
kobe shaw <kobeshaw330@gmail.com>
2023-09-09 13:08:05 -0500

    started to creat timer

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-document.querySelectorAll('.sidebar-item a').forEach(item => {
-    item.addEventListener('click', (e) => {
-        e.preventDefault();
-        const targetId = item.getAttribute('href').substring(1); // Remove the '#' symbol
-        const targetElement = document.getElementById(targetId);
-        
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - 750, // Adjust for header height
-                behavior: 'smooth',
-            });
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+
+const bitcoinHalvingDate = new Date(2024, 4, 7, 0, 0, 0).getTime();
+
+//update the time every second
+const countdown = document.getElementById('countdown');
+
+const updateCountdown = () => {
+    const now = new Date().getTime();
+    const timeRemaning = bitcoinHalvingDate - now;
+
+    if(timeRemaning <= 0){
+        countdown.innerHTML = "Bitcoin Halving is happening now!";
+    } else{
+       const days = Math.floor(timeRemaning / (1000 * 60 * 60 * 24)); 
+       const hours = Math.floor((timeRemaning % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); 
+       const minutes = Math.floor((timeRemaning % (1000 * 60 * 60 )) / (1000 * 60)); 
+       const seconds = Math.floor((timeRemaning % (1000 * 60)) / 1000); 
+
+       countdown.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+    }
+}
+
+updateCountdown();
+setInterval(updateCountdown, 1000);
+
 });
